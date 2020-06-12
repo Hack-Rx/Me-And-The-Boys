296f8940272859d6b3938312d16ee0d12b2023a4
saurabh13209 <agrawal99saurabh@gmail.com>
2020-06-12 18:35:32 +0530

    Video Sync

---
+++ src/Screens/RoomScreen.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, ActivityIndicator, Image, TouchableOpacity, Linking, Platform } from 'react-native';
+import Axios from 'axios';
+import { baseUrl, user, flaskUrl } from '../Store/Keys';
+import { FlatList } from 'react-native-gesture-handler';
+import { fontCustomSize } from '../function';
+import { storeData, getData } from '../Store/Storage';
+import { captureScreen } from "react-native-view-shot";
+
+
+export default RoomScreen = (props) => {
+
+    const [data, setData] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(true)
+        Axios.post(baseUrl + "/api/room/getRoom").then(res => {
+            setData(res.data)
+            setLoading(false)
+        })
+    }, [])
+
+    useEffect(() => {
+        Linking.getInitialURL().then(url => {
+            console.log(url)
+        });
+    })
+
+    return (
+        <View style={{ flex: 1, backgroundColor: "white" }}>
+            {
+                isLoading ? <View style={{ flex: 1 }}>
+                    <ActivityIndicator size="large" />
+                </View> : <View
+                    style={{ flex: 1 }}
+                >
+                        <FlatList
+                            data={data}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        getData(user).then(username => {
+                                            var tempUser = item.users;
+                                            console.log(tempUser)
+                                            var userArray = []
+                                            tempUser.forEach(user => {
+                                                userArray = [
+                                                    ...userArray,
+                                                    user["name"]
+                                                ]
+                                            })
+                                            if (userArray.includes(username)) {
+                                                props.navigation.navigate("Stream", { data: item })
+                                            } else {
+                                                tempUser = [
+                                                    ...tempUser,
+                                                    { name: username, role: "User" }
+                                                ]
+                                                Axios.post(baseUrl + "/api/room/addUser", {
+                                                    roomName: item.roomName,
+                                                    users: tempUser
+                                                }).then(() => {
+                                                    props.navigation.navigate("Stream", { data: item })
+                                                })
+                                            }
+                                        })
+                                    }}
+                                    style={{ flexDirection: "row", margin: 10, padding: 10, borderRadius: 15, borderColor: "#d4d4d4", borderWidth: 1 }}>
+                                    <View style={{ marginLeft: 10, justifyContent: "center", flex: 8 }}>
+                                        <Text style={{ fontSize: fontCustomSize(14), fontWeight: "bold", color: "black" }}>{item.roomName}</Text>
+                                        <Text style={{ fontSize: fontCustomSize(12), color: "#858585" }}>{item.users.length} viewer</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            )}
+                            keyExtractor={(item) => (item._id + "")}
+                        />
+
+                    </View>
+            }
+
+            <View
+                style={{ backgroundColor: '#252525', justifyContent: "center", alignItems: 'center', height: fontCustomSize(50), width: fontCustomSize(50), position: "absolute", bottom: fontCustomSize(30), borderRadius: fontCustomSize(50), right: fontCustomSize(30) }}
+            >
+                <TouchableOpacity
+                    onPress={() => {
+                        props.navigation.navigate("YoutubeLink")
+                    }}
+                    style={{ flex: 1, height: fontCustomSize(50), width: fontCustomSize(50), justifyContent: "center", alignItems: 'center' }}>
+                    <Text style={{ color: "white", fontSize: 40 }}>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
---
+++ src/mobx/ChatsStore.js
@@ -0,0 +1,45 @@
+import { observable, decorate } from 'mobx'
+
+var emoji1 = require('../../assests/image/heaty.png');
+
+class NotificationStore {
+    @observable videoData = {};
+    @observable queueData = [];
+
+    @observable users = [];
+    @observable role = "";
+
+    @observable emojiData = [
+        {
+            image: emoji1,
+            id: "1"
+        },
+        {
+            image: emoji1,
+            id: "2"
+        },
+        {
+            image: emoji1,
+            id: "3"
+        },
+        {
+            image: emoji1,
+            id: "4"
+        },
+        {
+            image: emoji1,
+            id: "5"
+        },
+        {
+            image: emoji1,
+            id: "6"
+        },
+    ];
+    @observable showEmoji = false;
+    @observable emojiId = 1;
+
+    @observable messageData = [];
+}
+
+
+export default new NotificationStore;
