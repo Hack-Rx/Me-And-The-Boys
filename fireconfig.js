b6f477910ca2d47b2a4708b41a9effc2da049024
rvstowrn <rvstowrn@gmail.com>
2020-06-13 06:28:20 +0530

    firsts

---
+++ fireconfig.js
@@ -0,0 +1,11 @@
+  var firebaseConfig = {
+	    apiKey: "AIzaSyDdbmuiIbe68_UcPAnjJlq16pmhhvZjMTA",
+	    authDomain: "vidsync-5e0dc.firebaseapp.com",
+	    databaseURL: "https://vidsync-5e0dc.firebaseio.com",
+	    projectId: "vidsync-5e0dc",
+	    storageBucket: "vidsync-5e0dc.appspot.com",
+	    messagingSenderId: "1059503992408",
+	    appId: "1:1059503992408:web:f498b1ebbae872c470e42d"
+	  };
+	  // Initialize Firebase
+	  firebase.initializeApp(firebaseConfig);
